cd7419e6509c0efdaf714672d45408b601f02b46
Brian "Beej Jorgensen" Hall <beej@beej.us>
2024-07-09 12:52:04 -0700

    thinner box lines

--- lua/latex_filters.lua
+++ lua/latex_filters.lua
@@ -8,13 +8,13 @@ end
 
 --- Wrap code blocks in tcolorbox environments
 function CodeBlock (cb)
-  return {raw_tex_block '\\begin{tcolorbox}[enhanced jigsaw,breakable,colback=white!97.2549!black]', cb, raw_tex_block '\\end{tcolorbox}'}
+  return {raw_tex_block '\\begin{tcolorbox}[enhanced jigsaw,breakable,colback=white!97.2549!black,boxrule=0.2mm]', cb, raw_tex_block '\\end{tcolorbox}'}
 end
 
 --- Add left border on blockquotes
 function BlockQuote (bq)
     return {
-        raw_tex_block '\\begin{tcolorbox}[enhanced jigsaw,breakable,drop shadow,breakable,arc=0pt,outer arc=0pt,grow to left by=-1cm,enlarge top by=0.1cm,enlarge bottom by=0.1cm]',
+        raw_tex_block '\\begin{tcolorbox}[enhanced jigsaw,breakable,drop shadow,breakable,arc=0pt,outer arc=0pt,grow to left by=-1cm,enlarge top by=0.1cm,enlarge bottom by=0.1cm,boxrule=0.2mm]',
         bq,
         raw_tex_block '\\end{tcolorbox}'
     }
